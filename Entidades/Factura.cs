dcd1571e95eb8e7db740770b0de192e4b0861509
gaston migone <gsmigone@gmail.com>
2022-11-03 13:45:32 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Factura.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Factura:DocumentoComercial
+    {
+        public string Tipo { get; set; }
+        
+    }
+}
---
+++ Entidades/Proveedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Proveedor:Persona
+    {
+        public string CUIT { get; set; }
+        public string Contacto { get; set; }
+        
+    }
+}
